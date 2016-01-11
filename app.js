50ec6c0e608774754adafad77e87c0c2e9a9276c
Robert-Rino <a901002666@gmail.com>
2016-01-12 05:23:45 +0800

    update for test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let cors = require('cors');
 let app = express();
 
 let routes = require('./routes/index.js');
-let pages = require('.routes/pages.js');
+// let pages = require('.routes/pages.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/v1', routes);
-app.use('api/v1/pages', pages);
+
+// app.use('api/v1/pages', pages);
 
 module.exports = app;
