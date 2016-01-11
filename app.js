84212edc237e887ec8707a44e58752986c11c45f
Robert-Rino <a901002666@gmail.com>
2016-01-12 05:58:58 +0800

    change index route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let cors = require('cors');
 let app = express();
 
 let routes = require('./routes/index.js');
+
 // let pages = require('.routes/pages.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,7 +18,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/v1', routes);
+app.use('/',routes);
+// app.use('/api/v1', routes);
 
 // app.use('api/v1/pages', pages);
 
