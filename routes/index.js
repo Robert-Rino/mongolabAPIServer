b604c0beaee96ac4b9caebf511820df2cf45b2bd
Robert-Rino <a901002666@gmail.com>
2016-01-13 00:09:34 +0800

    test process.env

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,8 @@ let router = express.Router();
 
 /* GET /api/v1 return version */
 router.get('/', (req, res) => {
-  res.send(process.env.TEST_STRING);
-
-  // res.json({
-  //   version: 'v1',
-  // });
+  res.json({
+    version: 'v1',
+  });
 });
 module.exports = router;
